b54ba369dfc6514ac4dd038d601ddec5426a47d2
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2024-10-30 13:58:24 +0000

    Updated on Wednessday

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { userRegister } from "../controllers/userControllers.js";
+import { remoteUpload } from "../middlewares/uploads.js";
 
 const userRouter = Router();
 
--- middlewares/uploads.js
+++ middlewares/uploads.js
@@ -1,4 +1,4 @@
-import multer from multer;
+import multer from "multer";
 import { multerSaveFilesOrg } from "multer-savefilesorg";
 
 export const remoteUpload = multer({
