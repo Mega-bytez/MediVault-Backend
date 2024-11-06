12ab5a4c9ba210530236d402490d03281df3233a
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2024-11-06 05:48:30 +0000

    Get By Vendor and All Users

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -3,8 +3,8 @@ import { addProductValidator } from "../validators/ProductValidators.js";
 
 export const addProduct = async (req, res, next) => {
   try {
-    if(!req.files || req.files.length === 0){
-        return res.status(404).json("No file added")
+    if (!req.files || req.files.length === 0) {
+      return res.status(404).json("No file added");
     }
     const { error, value } = await addProductValidator.validate({
       ...req.body,
@@ -14,10 +14,39 @@ export const addProduct = async (req, res, next) => {
       res.status(422).json(error);
     }
     const newProduct = await ProductModel.create({
-        ...value,
-        user: req.auth.id
+      ...value,
+      user: req.auth.id,
+    });
+    res.status(201).json(newProduct);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllProduct = async (req, res, next) => {
+  try {
+    const { filter = "{}", sort = "{}", limit = 100, skip = 0 } = req.query;
+    const product = await ProductModel.find(JSON.parse(filter))
+      .sort(JSON.parse(sort))
+      .limit(limit)
+      .skip(skip);
+    return res.status(200).json(product);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const vendorProduct = async (req, res, next) => {
+  try {
+    const { filter = "{}", sort = "{}", limit = 100, skip = 0 } = req.query;
+    const product = await ProductModel.find({
+      ...JSON.parse(filter),
+      user: req.auth.id,
     })
-    res.status(201).json(newProduct)
+      .sort(JSON.parse(sort))
+      .limit(limit)
+      .skip(skip);
+    return res.status(200).json(product);
   } catch (error) {
     next(error);
   }
