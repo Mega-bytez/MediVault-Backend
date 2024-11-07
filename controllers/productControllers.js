23e25563195022ad94d826178cc5b3cca81e8bcf
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2024-11-07 09:57:50 +0000

    GOoooooovenor

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -3,12 +3,12 @@ import { addProductValidator } from "../validators/ProductValidators.js";
 
 export const addProduct = async (req, res, next) => {
   try {
-    if (!req.files || req.files.length === 0) {
-      return res.status(404).json("No file added");
-    }
-    const { error, value } = await addProductValidator.validate({
+    // if (!req.files || req.files.length === 0) {
+    //   return res.status(404).json("No file added");
+    // }
+    const { error, value } = addProductValidator.validate({
       ...req.body,
-      image: req.files.map((file) => file.filename),
+      image: req?.files?.map((file) => file.filename),
     });
     if (error) {
       res.status(422).json(error);
