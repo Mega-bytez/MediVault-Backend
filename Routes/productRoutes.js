37f3053caafcdd263a9ada7e40529f3958db0869
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2024-11-07 10:17:10 +0000

    Govenor fixed

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -1,14 +1,12 @@
 import { Router } from "express";
 import { addProduct, getAllProduct, vendorProduct } from "../controllers/productControllers.js";
 import { isAuthenticated } from "../middlewares/auth.js";
-// import { remoteUpload } from "../middlewares/uploads.js";
+import { remoteUpload } from "../middlewares/uploads.js";
 
 const productRouter = Router()
 
-productRouter.post("/addProduct",  isAuthenticated, addProduct);
+productRouter.post("/addProduct",  isAuthenticated, remoteUpload.array("image", 10), addProduct);
 productRouter.get("/getAllProduct", isAuthenticated, getAllProduct);
 productRouter.get("/vendorPro", isAuthenticated, vendorProduct);
 
 export default productRouter;
-
-// remoteUpload.array("image", 10),
