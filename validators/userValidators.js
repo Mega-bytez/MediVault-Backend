ae415bb5943c89a700f59761c68a8358f53a5916
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2024-11-16 18:16:38 +0000

    Email with Joel

--- validators/userValidators.js
+++ validators/userValidators.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const userRegisterValidator = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().required(),
+  email: Joi.string().email().required(),
   licenseNumber: Joi.string().required(),
   registrationNumber: Joi.string().required(),
   mobileNumber: Joi.string().pattern(/^[0-9]+$/),
@@ -30,13 +30,13 @@ export const userRegisterValidator = Joi.object({
 });
 
 export const userLoginValidator = Joi.object({
-  email: Joi.string().required(),
+  email: Joi.string().email().required(),
   password: Joi.string().required(),
 });
 
 export const userUpdateValidator = Joi.object({
   name: Joi.string(),
-  email: Joi.string(),
+  email: Joi.string().email(),
   licenseNumber: Joi.string(),
   registrationNumber: Joi.string(),
   mobileNumber: Joi.string().pattern(/^[0-9]+$/),
